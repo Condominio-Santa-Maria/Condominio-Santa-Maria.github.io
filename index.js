2bbf1f86066ee1a8b28b1d785f7b5cd7f404680d
Roy Ruddy Paz Demiquel <ruddypazd@gmail.com>
2024-08-19 23:58:07 -0400

    otro

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const init=()=>{
-    document.getElementById("app").innerHTML = "<div>Bienvenido a Sant</div>";
+const init= async()=>{
+    let usuarios = await getUsuarios();
+    let cuerpo = "";
+    cuerpo += "<div>Bienvenido a Santa maria 1</div>";
+    cuerpo += "<div>"+JSON.stringify(usuarios)+"</div>";
+    document.getElementById("app").innerHTML = cuerpo;
+};
+
+const getUsuarios=()=>{
+    return new Promise(resolve=>{
+        fetch("https://ged579f8ad0fdcf-santamaria.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/").then(res => res.json()).then(obj => {
+            resolve(obj.data);
+        });
+    })
 };
 
 init();
