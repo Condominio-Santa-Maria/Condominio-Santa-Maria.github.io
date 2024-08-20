4fb89ea2a6d426d35339b9abedad8f54c47d6863
Roy Ruddy Paz Demiquel <ruddypazd@gmail.com>
2024-08-20 00:06:52 -0400

    agregando usuarios

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const init= async()=>{
-    let usuarios = await getUsuarios();
+    
     let cuerpo = "";
     cuerpo += "<div>Bienvenido a Santa maria 1</div>";
-    cuerpo += "<div>"+JSON.stringify(usuarios)+"</div>";
+    cuerpo += "<ul>";
+    cuerpo += "<li><a href='./usuarios/'>Usuarios</a></li>";
+    cuerpo += "</ul>";
     document.getElementById("app").innerHTML = cuerpo;
 };
 
-const getUsuarios=()=>{
-    return new Promise(resolve=>{
-        fetch("https://ged579f8ad0fdcf-santamaria.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/").then(res => res.json()).then(obj => {
-            resolve(obj.items);
-        });
-    })
-};
 
 init();
---
+++ usuarios/index.js
@@ -0,0 +1,17 @@
+const init= async()=>{
+    let usuarios = await getUsuarios();
+    let cuerpo = "";
+    cuerpo += "<div>Bienvenido a Santa maria 1</div>";
+    cuerpo += "<div>"+JSON.stringify(usuarios)+"</div>";
+    document.getElementById("app").innerHTML = cuerpo;
+};
+
+const getUsuarios=()=>{
+    return new Promise(resolve=>{
+        fetch("https://ged579f8ad0fdcf-santamaria.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/").then(res => res.json()).then(obj => {
+            resolve(obj.items);
+        });
+    })
+};
+
+init();
